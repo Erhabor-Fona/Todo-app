c5c2fb84b5e2ddf5eee782c20b1fab862e3292b6
Ehabor-Fona <osaofonaerhabor@gmail.com>
2021-10-08 03:56:39 +0100

    Feature: Task screen ready

---
+++ lib/Screens/task.screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  const TaskScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: Padding(
+                padding: EdgeInsets.only(
+                    top: 60.0, left: 30.0, bottom: 30.0, right: 30.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    CircleAvatar(
+                      child: Icon(
+                        Icons.list,
+                        size: 30.0,
+                        color: Colors.lightBlueAccent,
+                      ),
+                      radius: 30,
+                      backgroundColor: Colors.white,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      'Big Dee',
+                      style: TextStyle(
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    ),
+                    Text(
+                      '12 of 15 tasks left',
+                      style: TextStyle(color: Colors.white),
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    )
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
