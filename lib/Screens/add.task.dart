b44ff49fd8985f4d7023a7a990466bc11d85b57f
Ehabor-Fona <osaofonaerhabor@gmail.com>
2021-10-08 07:25:05 +0100

    Update: Finished add task screen

--- lib/Screens/add.task.dart
+++ lib/Screens/add.task.dart
@@ -13,6 +13,43 @@ class AddTask extends StatelessWidget {
           borderRadius: BorderRadius.only(topRight: Radius.circular(20),
           topLeft: Radius.circular(20),)
         ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 50.0, bottom: 25.0, left: 25.0, right: 25.0),
+              child: Text('Add Task',
+              style: TextStyle(fontSize: 30.0,
+              color: Colors.lightBlueAccent,
+              ),),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                  ),
+                ),),
+            SizedBox(height: 15.0,),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, left: 30.0, right: 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextButton(
+                    child: Text("Add"),
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+
+          ],
+        ),
       ),
     );
   }
